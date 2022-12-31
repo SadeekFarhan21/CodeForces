7dc4d71c58fe5b32f3e4a2d403e8c13bb15e939b
SadeekFarhan21 <farhansadeek019@gmail.com>
2022-12-31 09:01:21 -0500

    Die Roll

--- A_Die_Roll.cpp
+++ A_Die_Roll.cpp
@@ -2,7 +2,16 @@
 using namespace std;
 
 int main(){
-    int y, w; cin >> y >> w;
-    
+    vector <int> v(2); cin >> v[0] >> v[1];
+    sort(v.begin(), v.end());
+    map <int, string> m = {
+        {1, "1/1"},
+        {2, "5/6"},
+        {3, "2/3"},
+        {4, "1/2"},
+        {5, "1/3"},
+        {6, "1/6"}
+    };
+    cout << m[v[1]] << "\n";
     return 0;
 }
